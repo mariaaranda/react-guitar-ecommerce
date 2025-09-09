9b62bffe93c531f37c92e84f5511cdace4df8c57
María Aranda <84066617+mariaaranda@users.noreply.github.com>
2025-09-09 16:48:53 +0200

    First Commit - Upload App

---
+++ README.md
@@ -0,0 +1,15 @@
+# Ecommerce de guitarras
+Proyecto creado en Vite + React + TypeScript + Tailwind CSS
+
+## 🚀 Demo
+
+[¡Pruébame aquí!](https://aranda-react-guitar-ecommerce.netlify.app/)
+
+## 🛠️ Tecnologías
+
+- **Frontend**: [React 19](https://reactjs.org/) con TypeScript
+- **Estilos**: [TailwindCSS 4](https://tailwindcss.com/)
+- Uso de Custom Hook con useState
+- Uso de useMemo para optimizar cálculos derivados del state
+- Gestión de Local Storage
+- Limitación de compra a 5 unidades
---
+++ src/hooks/useCart.ts
@@ -0,0 +1,87 @@
+import { useState, useEffect } from 'react'
+import { db } from "../data/db"
+import { useMemo } from "react"
+import type { Guitar, CartItem, GuitarID } from "../types"
+
+
+export const useCart = () => {
+  
+  const initialCart = () : CartItem[] => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
+  const [data] = useState(db)
+  const [cart, setCart] = useState(initialCart)
+  
+  const MIN_ITEMS = 1
+  const MAX_ITEMS = 5
+  
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }, [cart])
+  
+  function addToCart(item: Guitar) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id )
+    if(itemExists >= 0) {
+      if(cart[itemExists].quantity >= MAX_ITEMS) return
+      const updateCart = [...cart]
+      updateCart[itemExists].quantity++
+      setCart(updateCart)
+    } else {
+      const newItem : CartItem = {...item, quantity : 1 }
+      setCart([...cart, newItem])
+    }
+  }
+
+  function removeFromCart(id : GuitarID) {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id))
+  }
+
+  function decreaseQuantity(id : GuitarID) {
+    const updateCart = cart.map( item => {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function increaseQuantity(id : GuitarID) {
+    const updateCart = cart.map( item => {
+      if (item.id === id && item.quantity < MAX_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity + 1
+        }
+      }
+      return item
+    })
+    setCart(updateCart)
+  }
+
+  function clearCart() {
+    setCart([])
+  }
+
+  
+  // State Derivado
+  const isEmpty = useMemo(() => cart.length === 0, [cart])
+  const cartTotal = useMemo(() => cart.reduce((total, item ) => total + (item.quantity * item.price), 0))
+
+  return {
+    data,
+    cart,
+    addToCart,
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity,
+    clearCart,
+    isEmpty,
+    cartTotal
+  }
+}
